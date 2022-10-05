a5b285b1dd5a033d5c6054f1a3e91808d9c0acdc
gustavo-arana <gustavo.arana91@gmail.com>
2022-10-05 15:49:37 -0500

    Maquetacipon y Estilos

---
+++ src/TodoCounter.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './styles/TodoCounter.css';
+
+function TodoCounter() {
+    return (
+        <h2 className="TodoCounter">has completado 2 de 3 to-do</h2>
+    );
+}
+
+export { TodoCounter };
